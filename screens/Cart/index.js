1a15ae8853a5c637d480ae28621b7621699c40fb
angel daniel <daniel_1234@hotmail.es>
2020-12-18 20:53:09 -0400

    Front End de Carrito

--- screens/Cart/index.js
+++ screens/Cart/index.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { Image, Alert } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right, Input, Spinner } from 'native-base';
+import { Block } from "galio-framework";
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right, Badge, List, ListItem } from 'native-base';
 import { API, graphqlOperation, Storage } from 'aws-amplify';
 import { createRequest, createRequestProduct, createRequestCustomer, updateRequestProduct } from "../../graphql/mutations";
 import { listRequestsForProducts } from "../../graphql/customQueries";
+
+import _default from "../../images/default-image.png";
+
+import CartItems from "../../components/CartItems";
 
 import { sendNotifications } from '../../constants/functions'; 
 
@@ -42,8 +47,8 @@ const Cart = (props) => {
     useEffect(() => {
         async function fetchData() {
             try {
-              //const img = await getImageFromStorage(props.route.params.product.product.image);
-              //setImage(img);
+              const img = await getImageFromStorage('OFFICES_PROFILE_IMAGES/4cef4c33-d515-4245-bd0e-21c98f016455.jpeg');
+              setImage(img);
             } catch (e) {
               console.log(e);
             }
@@ -51,11 +56,73 @@ const Cart = (props) => {
         fetchData();
 
     }, [getImageFromStorage]);
+
+    var office = {
+        id: 'item.id',
+        title: 'Alcoholado',
+        image: 'PRODUCTS_IMAGES/1607660366760_Alcoholado.jpeg',
+        cta: 'Entrar', 
+        horizontal: true,
+        supplier: false,
+        cost: "140"
+    };
+
+    var office1 = {
+      id: 'item.id',
+      title: 'Neck Paper',
+      image: 'PRODUCTS_IMAGES/1607734206667_Neck_Paper.jpeg',
+      cta: 'Entrar', 
+      horizontal: true,
+      supplier: false,
+      cost: "140"
+  };
   
     return (
       <Container>
+        <Header />
         <Content>
-            <Text>Soy el Carrito</Text>          
+          <Card>
+            <CardItem>
+              <Text>Subtotal (3 Items):</Text>
+              <Badge style={{marginLeft: 4}} danger>
+                <Text>RD$ 400</Text>
+              </Badge>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri: image}} />
+                <Body>
+                  <Text>Angel Barber Supplier</Text>
+                  <Text note>George David Mateo Rosario</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+                <Content>
+                  <Button rounded block warning>
+                    <Text>Enviar Solicitud</Text>
+                  </Button>
+                </Content>
+            </CardItem>
+            <CardItem cardBody>
+              <Block flex style={{padding: 5}}>
+                <CartItems item={office} horizontal/>
+                <CartItems item={office1} horizontal/>
+              </Block>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button warning rounded>
+                  <Text>Solicitar</Text>
+                </Button>
+              </Left>
+              <Right>
+                <Button danger rounded>
+                  <Text>Cancelar</Text>
+                </Button>
+              </Right>
+            </CardItem>
+          </Card>
         </Content>
       </Container>
     );
